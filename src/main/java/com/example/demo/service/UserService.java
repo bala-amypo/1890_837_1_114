aa6e0ede4c735ac4fdeae8f0df4e972f0555bfbc
bala-amypo <bala@amypo.in>
2025-12-23 07:51:10 +0000

    User commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,25 +1,25 @@
-package com.example.demo.service;
+// package com.example.demo.service;
 
-import com.example.demo.entity.User;
-import com.example.demo.repository.UserRepository;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.stereotype.Service;
+// import com.example.demo.entity.User;
+// import com.example.demo.repository.UserRepository;
+// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+// import org.springframework.stereotype.Service;
 
-@Service
-public class UserService {
+// @Service
+// public class UserService {
 
-    private final UserRepository repo;
-    private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//     private final UserRepository repo;
+//     private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
-    public UserService(UserRepository repo) {
-        this.repo = repo;
-    }
+//     public UserService(UserRepository repo) {
+//         this.repo = repo;
+//     }
 
-    public User register(User user) {
-        if (repo.findByEmail(user.getEmail()).isPresent()) {
-            throw new RuntimeException("Email already exists");
-        }
-        user.setPassword(encoder.encode(user.getPassword()));
-        return repo.save(user);
-    }
-}
+//     public User register(User user) {
+//         if (repo.findByEmail(user.getEmail()).isPresent()) {
+//             throw new RuntimeException("Email already exists");
+//         }
+//         user.setPassword(encoder.encode(user.getPassword()));
+//         return repo.save(user);
+//     }
+// }
