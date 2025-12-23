496cc30d48299b43a643e30fed04ad3d01686287
bala-amypo <bala@amypo.in>
2025-12-23 08:00:10 +0000

    User commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,25 +1,12 @@
-// package com.example.demo.service;
+import com.example.demo.service.UserService;
 
-// import com.example.demo.entity.User;
-// import com.example.demo.repository.UserRepository;
-// import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-// import org.springframework.stereotype.Service;
+@RestController
+@RequestMapping("/auth")
+public class AuthCounterController {
 
-// @Service
-// public class UserService {
+    private final UserService userService;
 
-//     private final UserRepository repo;
-//     private final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-
-//     public UserService(UserRepository repo) {
-//         this.repo = repo;
-//     }
-
-//     public User register(User user) {
-//         if (repo.findByEmail(user.getEmail()).isPresent()) {
-//             throw new RuntimeException("Email already exists");
-//         }
-//         user.setPassword(encoder.encode(user.getPassword()));
-//         return repo.save(user);
-//     }
-// }
+    public AuthCounterController(UserService userService) {
+        this.userService = userService;
+    }
+}
