e2368602f3853c0afcaa8ba335ecfa383f2bb18c
bala-amypo <bala@amypo.in>
2025-12-26 06:54:05 +0000

    User commit

--- src/main/java/com/example/demo/service/TokenServiceImpl.java
+++ src/main/java/com/example/demo/service/TokenServiceImpl.java
@@ -1,3 +1,15 @@
-Token token = new Token();
-token.setTokenValue("TOKEN_" + userId);
-return token;
+package com.example.demo.service;
+
+import com.example.demo.entity.Token;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TokenServiceImpl implements TokenService {
+
+    @Override
+    public Token generateToken(Long userId) {
+        Token token = new Token();
+        token.setTokenValue("TOKEN_" + userId);
+        return token;
+    }
+}
